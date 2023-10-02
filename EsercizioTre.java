643b03bc6783ae522e736346a4d856612cb88af7
MrMax01 <massimo.mauro00@gmail.com>
2023-10-02 17:58:13 +0200

    fix input

--- EsercizioTre.java
+++ EsercizioTre.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 
 public class EsercizioTre {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
         System.out.println("Inserisci la lunghezza dei due lati di un rettangolo!");
 
 
-        double lato1 = inputValidateDouble(1);
+        double lato1 = inputValidateDouble(1, in);
 
-        double lato2 = inputValidateDouble(2);
+        double lato2 = inputValidateDouble(2, in);
         System.out.println("Il Perimetro del rettangolo è " + perimetroRettangolo(lato1, lato2));
 
         System.out.println("Inserisci la lunghezza di 3 lati di un rettangolo!");
-        double catetoUno = inputValidateDouble(1);
-        double catetoDue = inputValidateDouble(2);
-        double catetoTre = inputValidateDouble(3);
+        double catetoUno = inputValidateDouble(1, in);
+        double catetoDue = inputValidateDouble(2, in);
+        double catetoTre = inputValidateDouble(3, in);
         System.out.println("L'area del triangolo è " + perimetroTriangolo(catetoUno, catetoDue, catetoTre));
         System.out.println("------------------------------------------------");
         System.out.println(pariDispari(7));
+        in.close();
+
     }
 
     public static double perimetroRettangolo(double lat1, double lat2) {
@@ -33,8 +35,8 @@ public class EsercizioTre {
         return Math.sqrt(p * (p - x) * (p - y) * (p - z));
     }
 
-    public static double inputValidateDouble(int n) {
-        Scanner in = new Scanner(System.in);
+    public static double inputValidateDouble(int n, Scanner in) {
+
         double number;
         do {
             System.out.println("Lato " + n + ":");
@@ -46,6 +48,8 @@ public class EsercizioTre {
             }
             number = in.nextDouble();
         } while (number <= 0);
+
+
         return number;
     }
 }
